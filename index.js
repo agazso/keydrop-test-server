9f9a5f0e9be3680060c70ce85698e87591737ea8
Attila Gazso <agazso@gmail.com>
2018-04-27 14:23:12 +0200

    Keydrop test server

---
+++ index.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ host: '0.0.0.0', port: 8080 });
+const clients = {};
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    console.log('received: %s', data);
+    const message = JSON.parse(data);
+
+    clients[message.sender] = ws;
+    if (clients.hasOwnProperty(message.recipient)) {
+        clients[message.recipient].send(data);
+    }
+  });
+  ws.on('close', function close(e) {
+    for (key in clients) {
+        const client = clients[key];
+        if (client == ws) {
+            console.log('closed %s', key);
+            delete clients[key];
+            break;
+        }
+    }
+  });
+});

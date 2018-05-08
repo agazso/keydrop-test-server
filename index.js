73256383dfcb6d6eec388648bcb4c9b71ac482b5
Attila Gazso <agazso@gmail.com>
2018-05-08 18:42:52 +0200

    Prettyprinting again

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const clients = {};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
-    console.log(new Date().toString(), 'received: ', JSON.stringify(data, null, 2));
     const message = JSON.parse(data);
+    console.log(new Date().toString(), 'received: ', JSON.stringify(message, null, 2));
 
     clients[message.sender] = ws;
     if (clients.hasOwnProperty(message.recipient)) {
